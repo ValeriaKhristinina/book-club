51f872048d9cc533f1af825046b47ae284f83b3c
Valeria Khristinina <valeria.khristinina@gmail.com>
2022-01-22 19:07:38 +0100

    created new page about all neetings

---
+++ src/components/meetings-page/meetings-page.tsx
@@ -0,0 +1,33 @@
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { getMeetingsWithAllInfo } from '../../store/selectors';
+import { formatDate } from "../../utils/utils";
+
+function MeetingsPage(): JSX.Element {
+  const meetingsUsual = useSelector(getMeetingsWithAllInfo)
+  const meetings = [...meetingsUsual].reverse();
+
+  return (
+    <section className="meetings-page container">
+      {meetings.map((meeting) => (
+        <Fragment key={meeting.id}>
+          <section className="meeting">
+            <div className="meeting__block">
+              <h2 className="meeting__book-title">{meeting.title}</h2>
+              <p className="meeting__book-author">{meeting.author}</p>
+            </div>
+            <div className="meeting__block">
+              <div className="meeting__block-date">{formatDate(meeting.chosenById.toString())}</div>
+            </div>
+            <div className="meeting__block">
+              <div className="meeting__block-chose-by">{meeting.chosenByUser?.firstName} {meeting.chosenByUser?.lastName}</div>
+            </div>
+          </section>
+        </Fragment>
+      ))}
+
+    </section>
+  )
+}
+
+export default MeetingsPage;
