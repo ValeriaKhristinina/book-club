c14b25a305fd455a592070cc24e3a28194342319
Valeria Khristinina <valeria.khristinina@gmail.com>
2022-01-22 17:32:50 +0100

    deleted mock-data

--- src/const.tsx
+++ src/const.tsx
@@ -1,41 +1,8 @@
-import { Participant } from "./types/participant";
-
 export enum AppRoute {
   Root = '/',
-  NewMeeting = '/new-meeting',
-  NextMeeting = '/next-meeting',
+  NextMeeting = '/meeting/next',
+  NewMeeting = '/meeting/new',
+  AllMeetings = '/meeting/all',
+  Meeting = '/meeting/:id',
   Participant = '/participant/:id'
 }
-
-export const PARTICIPANTS: Participant[] = [
-  {
-    id: 1,
-    firstName: 'Lera',
-    lastName: 'Khristinina',
-    joinDate: '2020-10-05',
-  },
-  {
-    id: 2,
-    firstName: 'Nikita',
-    lastName: 'Khristinin',
-    joinDate: '2020-10-05',
-  },
-  {
-    id: 3,
-    firstName: 'Alice',
-    lastName: 'Tarabukina',
-    joinDate: '2020-10-05',
-  },
-  {
-    id: 4,
-    firstName: 'Ekaterina',
-    lastName: 'Tarasevich',
-    joinDate: '2020-10-05',
-  },
-  {
-    id: 5,
-    firstName: 'Maria',
-    lastName: 'Urusova',
-    joinDate: '2020-12-05',
-  },
-]
