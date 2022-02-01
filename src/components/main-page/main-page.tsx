d46052887a51d711397590b43ed8ed320652d933
Valeria Khristinina <valeria.khristinina@gmail.com>
2022-02-01 14:41:14 +0100

    remove some in selectors

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -4,26 +4,27 @@ import { useState } from 'react';
 
 import './main-page.css';
 import { AppRoute } from '../../const';
-import { getParticipants, getMeetings } from '../../store/selectors';
-import { calculateAverageRating, checkVisitingParticipants, createNewQueque } from '../../utils/utils';
+import { getParticipants, getLastBook, getChoosingParticipant, getCompletedMeetings, getNextMeeting } from '../../store/selectors';
+import { calculateAverageRating, checkVisitingParticipants, createNewQueque, formatDate } from '../../utils/utils';
 import { useSelector } from 'react-redux';
 
 
 function MainPage(): JSX.Element {
   const [isActive, setActive] = useState(false)
-  const participants = useSelector(getParticipants);
-  const meetings = useSelector(getMeetings);
 
-  const choosingPerson = participants[2];
+  const participants = useSelector(getParticipants);
+  const meetings = useSelector(getCompletedMeetings);
+  const lastBook = useSelector(getLastBook)
+  const choosingPerson = useSelector(getChoosingParticipant)
+  const nextMeeting = useSelector(getNextMeeting)
+
   const lastFourMeetings = meetings.slice(-4);
   const visitingParticipants = checkVisitingParticipants(lastFourMeetings, participants);
+
   const newQueque = createNewQueque(choosingPerson, participants, visitingParticipants);
 
-  const lastBook = meetings[meetings.length - 1]
-  const lastChoosingParticipant = participants.find(person => person.id === lastBook.chosenById)
-  const nextChoosingParticipant = newQueque.find(person => person.id === lastBook.chosenById + 1)
-
-  calculateAverageRating(lastBook)
+  const lastChoosingParticipant = participants.find((person) => person.id === lastBook.chosenById)
+  const nextChoosingParticipant = newQueque[0]
 
   const clickActiveHandler = () => isActive ? setActive(false) : setActive(true)
 
@@ -35,17 +36,23 @@ function MainPage(): JSX.Element {
         <section className="main-content__block">
           <h3 className="main-content__title">Next meeting:</h3>
           <div>
-            <Link className="next-meeting-link" to={AppRoute.NextMeeting}><time>DATE</time></Link>
+            <time>{formatDate(nextMeeting?.date)}</time>
             <div><Link className="link-see-all" to={AppRoute.AllMeetings}>See all past meetings</Link></div>
           </div>
         </section>
         <section className="main-content__block">
           <h3 className="main-content__title">Will discuss:</h3>
-          <div>BOOK</div>
+          <div>
+            <Link className="next-meeting-link" to={AppRoute.NextMeeting}>{nextMeeting?.title}</Link>
+          </div>
         </section>
         <section className="main-content__block">
           <h3 className="main-content__title">Chosen by:</h3>
-          <div>NAME</div>
+          <div>
+            <Link to={`/participant/${choosingPerson?.id}`}>
+              {`${choosingPerson?.firstName} ${choosingPerson?.lastName}`}
+            </Link>
+          </div>
         </section>
         <section className="main-content__block main-content__block--all-width">
           <h3 className="main-content__title">Who choose next book :</h3>
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,11 +1,50 @@
+import moment from "moment";
+import { Meeting } from "../types/meeting";
 import { RootState } from "./store"
 
 export const getMeetings = (state: RootState) => state.meetings.meetings;
 export const getParticipants = (state: RootState) => state.participants.participants;
 export const getSingleParticipant = (state: RootState) => state.participants.singleParticipant;
-
 export const getMeetingDataLoaded = (state: RootState) => state.meetings.isDataLoaded
 export const getParticipantsDataLoaded = (state: RootState) => state.participants.isDataLoaded
+
+export const getCompletedMeetings = (state: RootState) => {
+  const meetings = state.meetings.meetings;
+  return meetings.filter((item) => item.isComplete === true)
+}
+
+export const getNextMeetings = (state: RootState) => {
+  const meetings = state.meetings.meetings;
+  return meetings.filter((item) => item.isComplete === false)
+}
+
+export const getLastBook = (state: RootState) => {
+  const completedMeetings = getCompletedMeetings(state)
+
+  return completedMeetings[completedMeetings.length - 1]
+}
+
+export const getNextMeeting = (state: RootState) => {
+  const nextMeetings = getNextMeetings(state)
+
+  const nextMeeting = nextMeetings.reduce((acc: Meeting, currentMeeting: Meeting): Meeting => {
+    if (moment(acc.date).isAfter(currentMeeting.date)) {
+      return currentMeeting
+    } else {
+      return acc
+    }
+  })
+
+  return nextMeeting
+}
+
+export const getChoosingParticipant = (state: RootState) => {
+  const nextMeeting = getNextMeeting(state)
+
+  const participants = state.participants.participants;
+  const participantID = nextMeeting?.chosenById
+  return participants.find((user) => user.id === participantID)
+}
 
 
 
