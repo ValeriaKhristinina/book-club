d9bb0dd80cd4d1cab10ed38dd6786d95859c87ff
Valeria Khristinina <valeria.khristinina@gmail.com>
2022-02-02 23:19:40 +0100

    created modal popup

--- src/components/meetings-page/meetings-page.tsx
+++ src/components/meetings-page/meetings-page.tsx
@@ -1,12 +1,37 @@
-import { Fragment } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import './meetings-page.css';
 import { getMeetingsWithAllInfo } from '../../store/selectors';
 import { formatDate } from "../../utils/utils";
+import { deleteMeetingAsync } from "../../store/meetings";
+import { Fragment, useState } from "react";
 
 function MeetingsPage(): JSX.Element {
+  const [isActiveModal, setActiveModal] = useState(false)
+  const [meetingID, setMeetingID] = useState(0)
   const meetingsUsual = useSelector(getMeetingsWithAllInfo)
   const meetings = [...meetingsUsual].reverse();
+  const dispatch = useDispatch();
+
+  const mainBody = document.querySelector('body')
+  const mainHtml = document.querySelector('html')
+
+  const manageScroll = (styleProperty: string) => {
+    if (mainBody) mainBody.style.overflow = styleProperty
+    if (mainHtml) mainHtml.style.overflow = styleProperty
+  }
+
+  if (isActiveModal) manageScroll('hidden')
+
+  const handleDelete = (meetingID: number) => {
+    dispatch(deleteMeetingAsync(meetingID))
+    setActiveModal(false)
+    manageScroll('visible')
+  }
+
+  const handleCloseModal = () => {
+    setActiveModal(false)
+    manageScroll('visible')
+  }
 
 
   return (
@@ -19,7 +44,7 @@ function MeetingsPage(): JSX.Element {
               <p className="meeting__book-author">{meeting.author}</p>
             </div>
             <div className="meeting__block">
-              <div className="meeting__block-date">{formatDate(meeting.chosenById.toString())}</div>
+              <div className="meeting__block-date">{formatDate(meeting.date)}</div>
             </div>
             <div className="meeting__block">
               <div className="meeting__block-chose-by">{meeting.chosenByUser?.firstName} {meeting.chosenByUser?.lastName}</div>
@@ -31,16 +56,35 @@ function MeetingsPage(): JSX.Element {
                   {
                     meeting.persons.map((person) => {
                       return (
-                        <li>{person.firstName} {person.lastName}</li>
+                        <li key={person.id}>{person.firstName} {person.lastName}</li>
                       )
                     })
                   }
                 </ul>
               </div>
             </div>
+            <div className="meeting__block">
+              <button onClick={() => { setMeetingID(meeting.id); setActiveModal(true); }} className="btn btn-delete" type="button">Delete Meeting</button>
+            </div>
           </section>
+
+
         </Fragment>
       ))}
+
+      {isActiveModal && (
+        <>
+          <div onClick={handleCloseModal} className="modal"></div>
+          <section className="warning">
+            <h1>Are you sure?</h1>
+            <div className="warning__btn-container">
+              <button onClick={() => { handleDelete(meetingID) }} className="btn">Yes</button>
+              <button onClick={handleCloseModal} className="btn btn--red">No</button>
+            </div>
+          </section>
+        </>
+      )
+      }
 
     </section>
   )
