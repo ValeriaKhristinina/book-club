60ea1450549a2d2166cdb6f60ae8bb2c77e0ec48
Valeria Khristinina <valeria.khristinina@gmail.com>
2022-02-14 16:29:53 +0100

    added new const about authorisation

--- src/const.tsx
+++ src/const.tsx
@@ -1,8 +1,15 @@
 export enum AppRoute {
   Root = '/',
+  Login = '/login',
   NextMeeting = '/meeting/next',
   NewForm = '/new-form/',
   AllMeetings = '/meeting/all',
   Meeting = '/meeting/:id',
   Participant = '/participant/:id'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
 }
