5fe6f9fa9709c2c39c79d2ee940f75b8a5fd70ca
Valeria Khristinina <valeria.khristinina@gmail.com>
2023-03-27 16:34:59 +0200

    added  form elements on meeting page

--- src/components/meeting-page/meeting-page.tsx
+++ src/components/meeting-page/meeting-page.tsx
@@ -1,31 +1,135 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getMeetingByIdAsync } from "../../store/meetings";
-import { getSingleMeeting } from "../../store/selectors";
-import { scrollToTop } from "../../utils/utils";
-import Page from "../page/page";
+import { Fragment, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getMeetingByIdAsync } from '../../store/meetings';
+import { getAllMembers, getSingleMeeting } from '../../store/selectors';
+import { scrollToTop } from '../../utils/utils';
+import Page from '../page/page';
+import {Meeting} from '../../types/meeting';
+import Thumbler from '../thumbler/thumbler';
 
 function MeetingPage() {
   scrollToTop();
   const params = useParams();
-  const meetingId = params.id
+  const meetingId = params.id;
+  const members = useSelector(getAllMembers);
+  const [isForm, setisForm] = useState(true);
+  const [bookName, setBookName] = useState('');
+  const [author, setAuthor] = useState('');
+  const [date, setDate] = useState('')
+  const [choosedBy, setChoosedBy] = useState(0)
+  const [isComplete, setIsComplite] = useState (false)
+  const [participant, setParticipant] = useState([])
+
+  const meeting = useSelector(getSingleMeeting);
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getMeetingByIdAsync(Number(meetingId)))
+    dispatch(getMeetingByIdAsync(Number(meetingId)));
   }, [dispatch, meetingId]);
 
-  const meeting = useSelector(getSingleMeeting)
+
+  if(!meeting) {
+    return <div> No meeting</div>
+  }
+
+
+ const handleSubmit = () => {
+  const changedMeeting: Meeting = {
+    id: meeting.id,
+    date: date || meeting.date,
+    title: bookName || meeting.title,
+    author: author || meeting.author,
+    cover: {
+      url: ''
+    },
+    chosenById: choosedBy || meeting.chosenById,
+    isComplete: isComplete || meeting.isComplete,
+    participants: participant || meeting.participants,
+
+  }
+
+  console.log('changedMeeting',changedMeeting)
+  return changedMeeting
+ } 
 
   return (
     <Page>
       <section className="meeting-page container">
-        <h1>{meeting?.title}</h1>
-        <button onClick={() => console.log(meeting)}>Click</button>
+        {!isForm ? (
+          <Fragment>
+            <h1>
+              {meeting?.title} by {meeting.author}
+            </h1>
+            <section>
+              {meeting?.participants.map((participant) => {
+                return (
+                  <>
+                    <div>
+                      {
+                        members.find((member) => member.id === participant.id)
+                          ?.firstName
+                      }{' '}
+                      {
+                        members.find((member) => member.id === participant.id)
+                          ?.lastName
+                      }
+                    </div>
+                  </>
+                );
+              })}
+            </section>
+          </Fragment>
+        ) : (
+          <Fragment>
+          <form action="submit">
+            <fieldset>
+              <label htmlFor="">Name</label>
+              <input
+                onChange={(e) => setBookName(e.target.value)}
+                type="text"
+                defaultValue={meeting.title}
+              />
+            </fieldset>
+
+            <fieldset>
+              <label htmlFor="">Author</label>
+              <input onChange ={(e)=> setAuthor(e.target.value)} type="text" defaultValue={meeting.author} />
+            </fieldset>
+
+            <fieldset>
+              <label htmlFor="">Choosed by</label>
+              <select onChange={(e)=> setChoosedBy(Number(e.target.value))} name="members" defaultValue={meeting.chosenById}>
+                {members.map((member) => (
+                  <option
+                    value={member.id}
+                  >
+                    {member.firstName} {member.lastName}
+                  </option>
+                ))}
+              </select>
+            </fieldset>
+
+            <fieldset>
+              <label htmlFor="">Meeting date</label>
+              <input onChange={(e) => setDate(e.target.value)} type="date" defaultValue={meeting?.date} />
+            </fieldset>
+
+            <fieldset>
+              <label htmlFor="">Participants and rating</label>
+              <fieldset>
+                <p>Lera Khristinina</p>
+                <div>12345</div>
+                <Thumbler booleanValue={true}/>
+              </fieldset>
+            </fieldset>
+          </form>
+          <button onClick={handleSubmit} type='submit'>Submit</button>
+          </Fragment>
+        )}
       </section>
     </Page>
-  )
+  );
 }
 
 export default MeetingPage;
